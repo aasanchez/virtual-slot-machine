87698f005f14a11b4231156b0a88fd7dba1235bf
Alexis Sanchez <aasanchez@gmail.com>
2018-09-07 13:32:39 +0200

    clean

--- app/Console/Commands/slotsCommand.php
+++ app/Console/Commands/slotsCommand.php
@@ -18,15 +18,11 @@ class slotsCommand extends Command
         $output = new Play();
         $board = new Board();
         $board->set_board(['J', 'J', 'J', 'J', 'K', 'cat', 'J', 'Q', 'monkey', 'bird', 'bird', 'bird', 'J', 'Q', 'A']);
-        //$board->set_board(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']);
         $output->board = $board->get_board();
-
         $paylines = new PayLines($output->board);
-
-        //$this->info(json_encode($paylines->getLines()));
         $output->paylines = $paylines->getWinners();
         $output->betAmount = 100;
-        $output->total_win = 40;
+        $output->total_win = 40
 
         $this->info($output->toJson(JSON_PRETTY_PRINT));
     }
